83bcea8d859143594eed84b975b94a9f51396bc0
Nathan LaFreniere <quitlahok@gmail.com>
2013-08-13 17:40:20 -0700

    add the messages field back

--- build.js
+++ build.js
@@ -4,19 +4,19 @@ var path = require('path');
 var riakProto = fs.readFileSync(path.join(__dirname, 'proto', 'riak.proto'), 'utf8');
 var riakKVProto = fs.readFileSync(path.join(__dirname, 'proto', 'riak_kv.proto'), 'utf8');
 var riakSearchProto = fs.readFileSync(path.join(__dirname, 'proto', 'riak_search.proto'), 'utf8');
-var output = { };
+var output = { messages: { } };
 
 proto2json.parse(riakProto, function (err, result) {
     Object.keys(result.messages).forEach(function (key) {
-        output[key] = result.messages[key];
+        output.messages[key] = result.messages[key];
     });
     proto2json.parse(riakKVProto, function (err, result) {
         Object.keys(result.messages).forEach(function (key) {
-            output[key] = result.messages[key];
+            output.messages[key] = result.messages[key];
         });
         proto2json.parse(riakSearchProto, function (err, result) {
             Object.keys(result.messages).forEach(function (key) {
-                output[key] = result.messages[key];
+                output.messages[key] = result.messages[key];
             });
             fs.writeFileSync('proto.json', JSON.stringify(output), 'utf8');
         });
