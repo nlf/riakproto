ae403ada6114e39226d37d8a52496d6910da99cc
Nathan LaFreniere <quitlahok@gmail.com>
2014-01-17 10:42:15 -0800

    riak 2.0.0-preview support

--- build.js
+++ build.js
@@ -1,24 +1,50 @@
 var proto2json = require('proto2json');
 var fs = require('fs');
 var path = require('path');
-var riakProto = fs.readFileSync(path.join(__dirname, 'proto', 'riak.proto'), 'utf8');
-var riakKVProto = fs.readFileSync(path.join(__dirname, 'proto', 'riak_kv.proto'), 'utf8');
-var riakSearchProto = fs.readFileSync(path.join(__dirname, 'proto', 'riak_search.proto'), 'utf8');
-var output = { messages: { } };
+var output = { messages: { }, codes: { } };
 
-proto2json.parse(riakProto, function (err, result) {
-    Object.keys(result.messages).forEach(function (key) {
-        output.messages[key] = result.messages[key];
-    });
-    proto2json.parse(riakKVProto, function (err, result) {
-        Object.keys(result.messages).forEach(function (key) {
+function parseFile(filename, callback) {
+    var key;
+    var filepath = path.join(__dirname, 'proto', filename);
+    var file = fs.readFileSync(filepath, 'utf8');
+    proto2json.parse(file, function (err, result) {
+        for (key in result.messages) {
             output.messages[key] = result.messages[key];
-        });
-        proto2json.parse(riakSearchProto, function (err, result) {
-            Object.keys(result.messages).forEach(function (key) {
-                output.messages[key] = result.messages[key];
+        }
+        callback();
+    });
+}
+
+function parseCsv(callback) {
+    var filepath = path.join(__dirname, 'proto', 'riak_pb_messages.csv');
+    var file = fs.readFileSync(filepath, 'utf8');
+    var lines = file.split('\n');
+    var i, l, line;
+    for (i = 0, l = lines.length; i < l; i++) {
+        line = lines[i].split(',');
+        if (line.length > 1) {
+            output.codes[line[0]] = line[1];
+            output.codes[line[1]] = line[0];
+        }
+    }
+    callback();
+}
+
+function writeFile() {
+    var filepath = path.join(__dirname, 'proto.json');
+    fs.writeFileSync(filepath, JSON.stringify(output), 'utf8');
+}
+
+parseFile('riak.proto', function () {
+    parseFile('riak_dt.proto', function () {
+        parseFile('riak_kv.proto', function () {
+            parseFile('riak_search.proto', function () {
+                parseFile('riak_yokozuna.proto', function () {
+                    parseCsv(function () {
+                        writeFile();
+                    });
+                });
             });
-            fs.writeFileSync('proto.json', JSON.stringify(output), 'utf8');
         });
     });
 });
