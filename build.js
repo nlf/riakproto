1dcfb3e7cd70431228bd6d759ba5b2c187c6b9a4
Nathan LaFreniere <quitlahok@gmail.com>
2014-01-20 16:17:32 -0800

    crudely add all message codes to the generated json

--- build.js
+++ build.js
@@ -6,6 +6,50 @@ var riakKVProto = fs.readFileSync(path.join(__dirname, 'proto', 'riak_kv.proto')
 var riakSearchProto = fs.readFileSync(path.join(__dirname, 'proto', 'riak_search.proto'), 'utf8');
 var output = { messages: { } };
 
+output.codes = {
+    '0': 'RpbErrorResp',
+    '1': 'RpbPingReq',
+    '2': 'RpbPingResp',
+    '3': 'RpbGetClientIdReq',
+    '4': 'RpbGetClientIdResp',
+    '5': 'RpbSetClientIdReq',
+    '6': 'RpbSetClientIdResp',
+    '7': 'RpbGetServerInfoReq',
+    '8': 'RpbGetServerInfoResp',
+    '9': 'RpbGetReq',
+    '10': 'RpbGetResp',
+    '11': 'RpbPutReq',
+    '12': 'RpbPutResp',
+    '13': 'RpbDelReq',
+    '14': 'RpbDelResp',
+    '15': 'RpbListBucketsReq',
+    '16': 'RpbListBucketsResp',
+    '17': 'RpbListKeysReq',
+    '18': 'RpbListKeysResp',
+    '19': 'RpbGetBucketReq',
+    '20': 'RpbGetBucketResp',
+    '21': 'RpbSetBucketReq',
+    '22': 'RpbSetBucketResp',
+    '23': 'RpbMapRedReq',
+    '24': 'RpbMapRedResp',
+    '25': 'RpbIndexReq',
+    '26': 'RpbIndexResp',
+    '27': 'RpbSearchQueryReq',
+    '28': 'RpbSearchQueryResp',
+    '29': 'RpbResetBucketReq',
+    '30': 'RpbResetBucketResp',
+    '40': 'RpbCSBucketReq',
+    '41': 'RpbCSBucketResp',
+    '50': 'RpbCounterUpdateReq',
+    '51': 'RpbCounterUpdateResp',
+    '52': 'RpbCounterGetReq',
+    '53': 'RpbCounterGetResp'
+}
+
+Object.keys(output.codes).forEach(function (key) {
+    output.codes[output.codes[key]] = Number(key);
+});
+
 proto2json.parse(riakProto, function (err, result) {
     Object.keys(result.messages).forEach(function (key) {
         output.messages[key] = result.messages[key];
